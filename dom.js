351b7f360343bbd7a286bb8dfdca9a6b573204e9
swatidurgekar <swatidurgekar@gmail.com>
2023-02-02 19:29:40 +0530

    first project

---
+++ dom.js
@@ -0,0 +1,16 @@
+//EXAMINE THE DOCUMENT OBJECT
+
+//console.dir(document);
+// 
+// console.log(document.images)
+
+var headerTitle=document.getElementById('header-title');
+var header=document.getElementById('main-header');
+//console.log(headerTitle);
+//headerTitle.textContent='Hello';
+//console.log(headerTitle.innerText)
+//headerTitle.innerHTML='<h3>Hello</h3>'
+header.style.borderBottom='solid 3px #000'
+var header1=document.querySelector('.title')
+header1.style.fontWeight='bold';
+header1.style.color='green';
