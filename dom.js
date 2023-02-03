4f67528303c9fc3e188f815ace1a0fd2d0985b07
swatidurgekar <swatidurgekar@gmail.com>
2023-02-03 19:34:27 +0530

    adding green background to items

--- dom.js
+++ dom.js
@@ -32,18 +32,50 @@
 
 
 
-var li=document.getElementsByTagName('li');
-console.log(li);
-console.log(li[1]);
-li[1].textContent='hello';
-li[1].style.fontWeight='bold'
-li[1].style.backgroundColor='yellow'
+// var li=document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent='hello';
+// li[1].style.fontWeight='bold'
+// li[1].style.backgroundColor='yellow'
 
 //items.style.backgroundColor='#f4f4f4'
 
-for(var i=0;i<li.length;i++){
-li[i].style.backgroundColor='#f4f4f4';
+// for(var i=0;i<li.length;i++){
+// li[i].style.backgroundColor='#f4f4f4';
+// }
+
+// var header=document.querySelector('#main-header');
+// header.style.borderBottom='solid 4px #ccc';
+
+// var input=document.querySelector('input');
+// input.value='hello world';
+
+// var submit=document.querySelector('input[type="submit"]');
+// submit.value='SEND';
+
+// var item=document.querySelector('.list-group-item');
+// item.style.color='red';
+
+// var lastitem=document.querySelector('.list-group-item:last-child');
+// lastitem.style.color="blue"
+
+var seconditem=document.querySelector('.list-group-item:nth-child(2');
+seconditem.style.color="coral";
+
+//QUERYSELECTOR
+var titles=document.querySelectorAll('.title');
+console.log(titles);
+
+var odd=document.querySelectorAll('li:nth-child(odd)');
+//var even=document.querySelectorAll('li:nth-child(even)');
+
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='green';
+    // even[i].style.backgroundColor='#ccc';
 }
 
-var header=document.querySelector('#main-header');
-header.getElementsByClassName.borderBottom='solid 4px #ccc';
+seconditem.style.backgroundColor='green';
+//seconditem.style.display='none'
+
+
