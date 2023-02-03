361336f51351327d31887b9c7a7a8bb1f3cd9d41
swatidurgekar <swatidurgekar@gmail.com>
2023-02-03 17:49:51 +0530

    added some more styles

--- dom.js
+++ dom.js
@@ -1,16 +1,36 @@
-//EXAMINE THE DOCUMENT OBJECT
+// //EXAMINE THE DOCUMENT OBJECT
 
-//console.dir(document);
-// 
-// console.log(document.images)
+// //console.dir(document);
+// // 
+// // console.log(document.images)
 
-var headerTitle=document.getElementById('header-title');
-var header=document.getElementById('main-header');
-//console.log(headerTitle);
-//headerTitle.textContent='Hello';
-//console.log(headerTitle.innerText)
-//headerTitle.innerHTML='<h3>Hello</h3>'
-header.style.borderBottom='solid 3px #000'
-var header1=document.querySelector('.title')
-header1.style.fontWeight='bold';
-header1.style.color='green';
+// var headerTitle=document.getElementById('header-title');
+ var header=document.getElementById('header-title');
+// //console.log(headerTitle);
+// //headerTitle.textContent='Hello';
+// //console.log(headerTitle.innerText)
+// //headerTitle.innerHTML='<h3>Hello</h3>'
+// header.style.borderBottom='solid 3px #000'
+// var header1=document.querySelector('.title')
+// header1.style.fontWeight='bold';
+// header1.style.color='green';
+// console.log(header.textContent)
+// console.log(header.innerText);
+
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent='hello';
+items[1].style.fontWeight='bold'
+items[1].style.backgroundColor='yellow'
+
+//items.style.backgroundColor='#f4f4f4'
+
+for(var i=0;i<items.length;i++){
+    items[i].style.backgroundColor='#f4f4f4'
+}
+
+items[2].style.backgroundColor='green'
+for(var i=0;i<items.length;i++){
+items[i].style.fontWeight='bold';
+}
