0b94a8a0afb7694781270625834f863d29d738f2
swatidurgekar <swatidurgekar@gmail.com>
2023-02-03 18:19:42 +0530

    get element by tag name

--- dom.js
+++ dom.js
@@ -5,7 +5,7 @@
 // // console.log(document.images)
 
 // var headerTitle=document.getElementById('header-title');
- var header=document.getElementById('header-title');
+ //var header=document.getElementById('header-title');
 // //console.log(headerTitle);
 // //headerTitle.textContent='Hello';
 // //console.log(headerTitle.innerText)
@@ -17,20 +17,33 @@
 // console.log(header.textContent)
 // console.log(header.innerText);
 
-var items=document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent='hello';
-items[1].style.fontWeight='bold'
-items[1].style.backgroundColor='yellow'
+// var items=document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent='hello';
+// items[1].style.fontWeight='bold'
+// items[1].style.backgroundColor='yellow'
+
+// //items.style.backgroundColor='#f4f4f4'
+
+// for(var i=0;i<items.length;i++){
+//     items[i].style.backgroundColor='#f4f4f4'
+// }
+
+
+
+var li=document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent='hello';
+li[1].style.fontWeight='bold'
+li[1].style.backgroundColor='yellow'
 
 //items.style.backgroundColor='#f4f4f4'
 
-for(var i=0;i<items.length;i++){
-    items[i].style.backgroundColor='#f4f4f4'
+for(var i=0;i<li.length;i++){
+li[i].style.backgroundColor='#f4f4f4';
 }
 
-items[2].style.backgroundColor='green'
-for(var i=0;i<items.length;i++){
-items[i].style.fontWeight='bold';
-}
+var header=document.querySelector('#main-header');
+header.getElementsByClassName.borderBottom='solid 4px #ccc';
