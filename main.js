fbd2e763081e5798a5dc9d5724d19e2f3306c824
swatidurgekar <swatidurgekar@gmail.com>
2023-02-04 16:58:21 +0530

    filtering

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
 var form=document.getElementById('addForm');
 var itemList=document.getElementById('items');
+var filter=document.getElementById('filter');
 
 
 form.addEventListener('submit',addItem);
 
 itemList.addEventListener('click',removeItem);
 
+filter.addEventListener('keyup',filterItems);
+
 function addItem(e){
     e.preventDefault();
 
     var newItem=document.getElementById('item').value;
-
+    var description=document.getElementById('description').value;
     //create new li
     var li=document.createElement('li');
     //Add class
     li.className='list-group-item';
    //Add text 
-   li.appendChild(document.createTextNode(newItem));
+   li.appendChild(document.createTextNode(newItem+' '));
+   li.appendChild(document.createTextNode(description));
 
 
    var deleteBtn=document.createElement('button')
@@ -45,4 +49,19 @@ function removeItem(e){
             itemList.removeChild(li);
         }
     }
+}
+
+function filterItems(e){
+    var text=e.target.value.toLowerCase();
+    var items=itemList.getElementsByTagName('li');
+    Array.from(items).forEach(function(item){
+var itemName=item.textContent;
+
+if(itemName.toLowerCase().indexOf(text)!=-1){
+item.style.display='block'
+}else{
+    item.style.display='none'
+}
+    })
+
 }
