3c8431e7243eb4285e2991d9da99c9ebc5215791
swatidurgekar <swatidurgekar@gmail.com>
2023-03-18 22:25:32 +0530

    showUserOnScreen

--- crudcrud/crud.js
+++ crudcrud/crud.js
@@ -15,9 +15,32 @@ phonenumber
 }
 const myObj_serialized=JSON.stringify(myObj);
 axios.post('https://crudcrud.com/api/6cbb6c090be24a5599e24f682796991c/appointmentData',myObj)
-.then((response)=>console.log(response))
+.then((resolve)=>console.log(resolve))
 .catch((err)=>console.log(err))
 
 
 //localStorage.setItem('form',myObj_serialized);
 }
+
+function showUserOnScreen(val){
+    const form=document.getElementById('form');
+    let ul=document.createElement('ul');
+    let li=document.createElement('li');
+    li.textContent=val;
+    ul.appendChild(li);
+    form.appendChild(ul);
+}
+
+
+window.addEventListener('DOMContentLoaded',()=>{
+    axios.get('https://crudcrud.com/api/6cbb6c090be24a5599e24f682796991c/appointmentData')
+    .then((resolve)=>{
+        console.log(resolve);
+        for(let i=0;i<resolve.data.length;i++){
+            showUserOnScreen(JSON.stringify(resolve.data[i]))
+        }
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
+})
